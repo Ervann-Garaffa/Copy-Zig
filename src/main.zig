c8cd3e89daab7d6d2ba01bfcde364f266d3440a3
Ervann-Garaffa <ervanngaraffa-botta@hotmail.fr>
2024-11-22 23:03:12 +0100

    Poup

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,40 @@
+// This whole program is copied from jdh making a game in zig Youtube video
+
 const std = @import("std");
 const expect = std.testing.expect;
 
 const rl = @import("raylib.zig");
 // TODO Particle simulation
+
+const Vector2 = rl.Vector2; // Alias type
+
+const THICKNESS = 3.0;
+
+const State = struct { shipPos: rl.Vector2 };
+var state: State = {};
+
+fn drawLines(origin: Vector2, scale: f32, points: []Vector2) void { // Slice ?!
+
+    const Transformer = struct {
+        fn apply(p: Vector2) Vector2 {
+            return Vector2{ .x = p.x + origin.x * scale };
+        }
+    };
+
+    for (0..points.len) |i| {
+        rl.DrawLineEx(
+            Transformer.apply(points[i]),
+            Transformer.apply(points[(i + 1) % points.len]),
+            THICKNESS,
+            .white,
+        );
+    }
+}
+
+// fn drawShip(pos: Vector2) void {
+//    _ = pos;
+//    rl.DrawLineEx();
+// }
 
 pub fn main() !void {
     std.debug.print("\n\nHello World!\n\n", .{});
